cfa48a88637f8fc70f819a07c2b47606fd174e68
kclc950 <dvitsios@gmail.com>
2019-10-02 20:13:49 +0000

    remove nltk dependency; add utf-encoding directive in mantis_profiler

--- mantis_ml/bin/mantis_ml_profiler.py
+++ mantis_ml/bin/mantis_ml_profiler.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 from collections import Counter
-from nltk.corpus import stopwords
 import argparse
 import sys, os
 import re
 
 from mantis_ml.modules.pre_processing.data_compilation.process_features_filtered_by_disease import ProcessFeaturesFilteredByDisease
 from mantis_ml.config_class import Config
+
+
 
 
 # Disable print to stdout
@@ -177,6 +179,13 @@ def assess_mgi_filtered_output(proc_obj, cfg, verbose=False):
     print(sorted(filtered_selected_mgi_phenotypes_expanded))
 
 
+
+def get_english_stopwords():
+    eng_stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+
+    return eng_stopwords
+
+
 if __name__ == '__main__':
 
 
@@ -198,7 +207,7 @@ if __name__ == '__main__':
     proc_obj = ProcessFeaturesFilteredByDisease(cfg)
 
     # common strings to exclude from profiling
-    eng_stopwords = set(stopwords.words('english'))
+    eng_stopwords = get_english_stopwords()
     custom_bullet = u'\u2022' * 5
     line_spacer = '\n' * 6
 
