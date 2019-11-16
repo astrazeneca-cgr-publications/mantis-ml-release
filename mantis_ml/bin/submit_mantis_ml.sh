6d198b079ca2fdc17e265f45c794e844342a3ace
kclc950 <dvitsios@gmail.com>
2019-11-16 12:15:04 +0000

    update mantis_ml SLURM submission script

--- mantis_ml/bin/submit_mantis_ml.sh
+++ mantis_ml/bin/submit_mantis_ml.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=20
 #SBATCH --mem-per-cpu=4G
+#SBATCH --time=12:0:0
 
 conf=$1
-mantis_ml -c $conf -n 10 -i 10
+mantis_ml -c $conf -n 20 -i 10
 
