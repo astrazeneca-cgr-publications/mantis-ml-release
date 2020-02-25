a86b57352d845874034636efda5903e0bb378ed5
kclc950 <dvitsios@gmail.com>
2020-02-25 15:38:32 +0000

    update enrichment module for benchmarking of external methods, against known and/or novel genes

---
+++ mantis_ml/modules/hypergeom_enrichment/submit_enrichment_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=40G
+#SBATCH --time=24:0:0
+
+# mantis-ml
+python __main__.py -c ../../conf/ALS_config.yaml -o ../../../out/ALS-production-Full/ -e ../../../misc/overlap-collapsing-analyses/ALS_Science_2015/ALS/Dom_LoF_collapsing_ranking.ALS.no_quotes.csv -x 41 -y 60
+
+python __main__.py -c ../../conf/Epilepsy_config.yaml -o ../../../out/Epilepsy-production-Full/ -e ../../../misc/overlap-collapsing-analyses/Epilepsy-LancetNeurology_2017/GGE/primary_collapsing_ranking.GGE.csv -x 42 -y 60
+
+python __main__.py -c ../../conf/CKD_config.yaml -o ../../../out/CKD-production-Full/ -e ../../../misc/overlap-collapsing-analyses/CKD_JASN_2019/CKD/v-AURORA-CUMC-all_dom_ultrarare_OO_collapsing_ranking.CKD.csv -x 67 -y 60
+
+
+# Phenolyzer  -- (similarly for ToppGene and ToppNet, just replacing the Phenolyzer arg)
+python __main__.py -c ../../conf/CKD_config.yaml -o ../../../out/CKD-production-Full/ -e ../../../misc/overlap-collapsing-analyses/CKD_JASN_2019/CKD/v-AURORA-CUMC-all_dom_ultrarare_OO_collapsing_ranking.CKD.csv -x 67 -y 60 -b Phenolyzer -p CKD
+
+python __main__.py -c ../../conf/ALS_config.yaml -o ../../../out/ALS-production-Full/ -e ../../../misc/overlap-collapsing-analyses/ALS_Science_2015/ALS/Dom_LoF_collapsing_ranking.ALS.no_quotes.csv -x 41 -y 60 -b Phenolyzer -p ALS
+
+python __main__.py -c ../../conf/Epilepsy_config.yaml -o ../../../out/Epilepsy-production-Full/ -e ../../../misc/overlap-collapsing-analyses/Epilepsy-LancetNeurology_2017/GGE/primary_collapsing_ranking.GGE.csv -x 42 -y 60 -b Phenolyzer -p Epilepsy
